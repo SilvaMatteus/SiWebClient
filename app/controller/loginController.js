251dca2cab024f7d3f33d70541cc8791bfc28db9
Matteus Silva <silvamatteus@lsd.ufcg.edu.br>
2016-07-24 14:19:25 -0300

    us 01

---
+++ app/controller/loginController.js
@@ -0,0 +1,18 @@
+/**
+* Login Controller
+*/
+app.controller('loginController', loginController);
+
+function loginController($scope, $timeout) {
+    $scope.count = 1024;
+    $scope.perc = 25.5;
+
+    $scope.increment = function() {
+      $scope.count++;
+      $scope.perc += 0.3;
+      if ($scope.count > 0) {
+        $timeout($scope.increment, 1000);
+      }
+    };
+    $scope.increment();
+}
