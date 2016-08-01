ceb14aef0a20586d56107c7158f1fd0ac4d79f12
Matteus Silva <silvamatteus@lsd.ufcg.edu.br>
2016-08-01 07:14:47 -0300

    Trying to add login

--- app/controller/userController.js
+++ app/controller/userController.js
@@ -3,7 +3,7 @@
 */
 app.controller('userController', userController);
 
-function userController($scope, $timeout) {
+function userController($scope, $timeout, User, notificationFactory) {
     $scope.count = 1024;
     $scope.perc = 25.5;
 
@@ -15,4 +15,24 @@ function userController($scope, $timeout) {
       }
     };
     $scope.increment();
+
+    // $scope.$watch("listOfUser", function(newValue, oldValue) {
+    //     console.log(" mudou  " , newValue, oldValue);
+    // });
+
+    $scope.login = function() {
+        $scope.listOfUser = User.query()
+        console.log($scope.listOfUser.length);
+        for (var i = 0; i < $scope.listOfUser.length; i++) {
+            if ($scope.listOfUser[i].email === "email@email.com") {
+                console.log($scope.userLoggedIn, 'oiiiiii');
+                $scope.userLoggedIn = $scope.listOfUser[i];
+            }
+        }
+        // angular.forEach($scope.listOfUser, function(user, key) {
+        //   if (user.email === "email@email.com") {
+        //     $scope.userLoggedIn = user;
+        //   }
+        // });
+    }
 }
