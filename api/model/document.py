4d61327ee74844427943f4a0ba2d95e5d81c2dcc
Matteus Silva <silvamatteus@lsd.ufcg.edu.br>
2016-09-09 15:38:37 -0300

    Adding documentation to Document class.

--- api/model/document.py
+++ api/model/document.py
@@ -2,10 +2,16 @@
 
 import uuid
 
+""" This module contains the Document class of the Reader's model.
+"""
+
 class Document(object):
-    """ Object that represents a document of Reader """
+    """ Object that represents a document of Reader
+    """
 
     def __init__(self, name, extension, content):
+        """ Constructor of Document.
+        """
         super(Document, self).__init__()
         self.name = name
         self.extension = extension
@@ -13,6 +19,8 @@ class Document(object):
         self.id = str(uuid.uuid4()).replace('-', '')
 
     def __eq__(self, other):
+        """ Equals method of a Document.
+        """
         if isinstance(other, Document):
             return self.id == other.id
         return False
